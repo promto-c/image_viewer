c99af765b7cad337321a21af89e1b218e717b2a9
promto-c <prommm01@gmail.com>
2023-08-20 22:41:19 +0700

    test b spline

--- shaders/viewer_fragment.glsl
+++ shaders/viewer_fragment.glsl
@@ -3,12 +3,19 @@ uniform sampler2D imageTexture;
 uniform float lift;
 uniform float gamma;
 uniform float gain;
-in vec2 TexCoords;
+uniform bool isTexture; // Indicates if the current drawing is based on texture sampling or not
+uniform vec4 color;
 
+
+in vec2 TexCoords;
 out vec4 FragColor;
 
 void main() {
-    vec4 color = texture(imageTexture, TexCoords);
-    color.rgb = pow(color.rgb * gain + lift, vec3(1.0/gamma));
-    FragColor = color;
+    if (isTexture) {
+        vec4 color = texture(imageTexture, TexCoords);
+        color.rgb = pow(color.rgb * gain + lift, vec3(1.0/gamma));
+        FragColor = color;
+    } else {
+        FragColor = color;
+    }
 }
